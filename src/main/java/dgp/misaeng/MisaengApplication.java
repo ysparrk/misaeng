a2adabfb6b65cd6be36e7bc4caf9cfa085dd86f7
ysparrk <youngseo0703@gmail.com>
2024-11-13 22:37:32 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/dgp/misaeng/MisaengApplication.java
@@ -0,0 +1,13 @@
+package dgp.misaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgp/misaeng/MisaengApplicationTests.java
@@ -0,0 +1,13 @@
+package dgp.misaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
