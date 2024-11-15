e5263642fc7b804df9f7c6a244b9f4d725374631
ysparrk <youngseo0703@gmail.com>
2024-11-15 10:20:18 +0900

    feat: 엔티티 셋팅

---
+++ src/main/java/dgp/misaeng/domain/member/entity/Member.java
@@ -0,0 +1,56 @@
+package dgp.misaeng.domain.member.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SQLDelete(sql = "UPDATE member SET deleted_at = now() WHERE member_id = ?")
+@SQLRestriction("isDeleted = false")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "nickname", length = 12, nullable = false)
+    private String nickname;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @ColumnDefault("false")
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public Member(Long id, String email, String nickname, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isDeleted, LocalDateTime deletedAt) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.isDeleted = isDeleted;
+        this.deletedAt = deletedAt;
+    }
+}
---
+++ src/main/java/dgp/misaeng/domain/microbe/entity/Microbe.java
@@ -0,0 +1,56 @@
+package dgp.misaeng.domain.microbe.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "microbe")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SQLDelete(sql = "UPDATE microbe SET deleted_at = now() WHERE microbe_id = ?")
+@SQLRestriction("isDeleted = false")
+public class Microbe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "microbe_id", updatable = false)
+    private Long microbeId;
+
+    @Column(name = "microbe_name", length = 30, nullable = false)
+    private String microbeName;
+
+    @Column(name = "rgb_stat", length = 30, nullable = false)
+    private String rgbStat;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @ColumnDefault("false")
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public Microbe(Long microbeId, String microbeName, String rgbStat, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isDeleted, LocalDateTime deletedAt) {
+        this.microbeId = microbeId;
+        this.microbeName = microbeName;
+        this.rgbStat = rgbStat;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.isDeleted = isDeleted;
+        this.deletedAt = deletedAt;
+    }
+}
